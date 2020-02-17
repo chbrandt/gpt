7e6fde815abafccbe9b82d2cedb87ac63c24a619
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-02-17 08:21:26 +0100

    Update Geopkg

--- gpt/_geopkg.py
+++ gpt/_geopkg.py
@@ -38,11 +38,24 @@ class _GeopkgInterface:
 class Geopkg(_GeopkgInterface):
     def __init__(self, data=None):
         super().__init__()
+        self.data = data
+
+    # def __getattribute__(self, attr):
+    #     if attr in self.data.keys():
+    #         return self.data.__getitem__(attr)
+    #     else:
+    #         return
 
     @staticmethod
     def read_file(filename):
         data = utils.read_geopackage(filename)
         return Geopkg(data)
+
+    def list(self, sort=True):
+        layers = self.data.keys()
+        if sort:
+            layers = sorted(layers)
+        return list(layers)
 
     def rename_layer(self, name_old, name_new):
         self.data[name_new] = self.data[name_old]
