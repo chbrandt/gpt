7e933d166eebaa07be83c85173289a2e027c3389
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-05-14 14:25:32 +0200

    [wip] add rename_field method

--- gpt/_geopkg.py
+++ gpt/_geopkg.py
@@ -66,7 +66,65 @@ class Geopkg(_base.GeopkgBase):
         self.data[name_new] = self.data[name_old]
         del self.data[name_old]
         self._fix_style_layer_name_changed(name_old, name_new)
+        return self
 
+    def _ensure_just_one_default_style(style_table, layer_name):
+        """perform some checks on the style table and clean if possible"""
+        ids = list(styles.loc[styles["f_table_name"] == layer_name].index) # ids of the possible style for this layer
+        defaults = list(styles.loc[ ids, "useAsDefault"]) # the default flag for these rows
+        
+        if len(ids) == 0:
+            log.error("no style for this layer found")
+            return
+        
+        if len(ids) == 1:
+            id = ids[0] # use that as style
+            
+        else: # we select the first style that is also set as default
+            try:
+                first_default  = defaults.index(True)
+                id = ids[first_default]
+            except: # no style set as default
+                log.warning("No styles with default flag. using as style the last inserted. may be a problem!")
+                id = ids[-1] # chose the last inserted
+        
+        styles.loc[ids, "useAsDefault"] = False # clear default flags, just to be sure, we may actually want to delete those unused styles
+        styles.loc[id, "useAsDefault"] = True # now set the default style
+        
+    
+    def rename_field(self:_base.GeopkgBase, layer_name, old_field_name, new_field_name):
+        # classical change
+        layer = self[layer_name]
+        layer.rename(columns={old_field_name:new_field_name}, inplace=True)
+        
+        # fix the style table
+        styles = self["layer_styles"] # extract the style table - we should check whether it exists or not btw
+        
+        # these would not be needed actually, just an useful check
+        _ensure_just_one_default_style(styles, layer_name)
+        
+        # now we want to alter the qml content for all the styles connected with this layers
+        ids = list(styles.loc[styles["f_table_name"] == layer_name].index)
+        for id in ids:
+            qml = styles.loc[id, "styleQML"]
+    
+            import xml.etree.ElementTree as ET
+            tree = ET.fromstring(qml)
+            renderer = tree.find("renderer-v2")
+            
+            if renderer.attrib["attr"] == old_field_name: # the field was used in this style
+                renderer.attrib["attr"] = new_field_name
+                newstyle = str(ET.tostring(tree, encoding='unicode', method='xml'))
+                styles.loc[id,"styleQML"] = newstyle
+            else: # nothing to do - not affected
+                log.debug("the field were not used as filter for the style")
+                return self
+    
+            # clear this because we cannot grant 1 to 1 with qml for now
+            styles.loc[id, "styleSLD"] = ' '
+            return self
+    
+   
     def _read_gpkg(self, filename):
         assert all(a in self._tempdir for a in ['path','generated'])
         tempdir = self._tempdir
