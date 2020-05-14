621f97ddafee57f47538a9bb5cbc38527816bae2
luca from muntzer <luca.penasa@gmail.com>
2020-05-14 16:33:27 +0200

    sld should be fixed and also qml rule based

--- gpt/_geopkg.py
+++ gpt/_geopkg.py
@@ -1,42 +1,11 @@
 import os
-import sys
-import shutil
-import collections
-
-import fiona
-import geopandas
 
 from . import _base
 from . import utils
 from .log import *
+from .styling import fix_qml_style_on_field_name_change, fix_sld_style_on_field_name_change, ensure_just_one_default_style
 
 
-def _ensure_just_one_default_style(styles, layer_name):
-    """perform some checks on the style table and clean if possible"""
-    ids = list(styles.loc[styles["f_table_name"] == layer_name].index) # ids of the possible style for this layer
-    defaults = list(styles.loc[ ids, "useAsDefault"]) # the default flag for these rows
-    
-    if len(ids) == 0:
-        #log.error("no style for this layer found")
-        print("no style for this layer found")
-        return
-    
-    if len(ids) == 1:
-        id = ids[0] # use that as style
-        
-    else: # we select the first style that is also set as default
-        try:
-            first_default  = defaults.index(True)
-            id = ids[first_default]
-        except: # no style set as default
-            #log.warning("No styles with default flag. using as style the last inserted. may be a problem!")
-            print("No styles with default flag. using as style the last inserted. may be a problem!")
-            id = ids[-1] # chose the last inserted
-    
-    styles.loc[ids, "useAsDefault"] = False # clear default flags, just to be sure, we may actually want to delete those unused styles
-    styles.loc[id, "useAsDefault"] = True # now set the default style
-        
-    
 class Geopkg(_base.GeopkgBase):
     """
     Data:
@@ -94,37 +63,27 @@ class Geopkg(_base.GeopkgBase):
         self._fix_style_layer_name_changed(name_old, name_new)
         return self
 
-    def rename_field(self:_base.GeopkgBase, layer_name, old_field_name, new_field_name):
+    def rename_field(self: _base.GeopkgBase, layer_name, old_field_name, new_field_name):
         # classical change
         layer = self[layer_name]
-        layer.rename(columns={old_field_name:new_field_name}, inplace=True)
-        
+        layer.rename(columns={old_field_name: new_field_name}, inplace=True)
+
         # fix the style table
-        styles = self["layer_styles"] # extract the style table - we should check whether it exists or not btw
-        
+        styles = self["layer_styles"]  # extract the style table - we should check whether it exists or not btw
+
         # these would not be needed actually, just an useful check
-        _ensure_just_one_default_style(styles, layer_name)
-        
-        # now we want to alter the qml content for all the styles connected with this layers
+        # ensure_just_one_default_style(styles, layer_name)
+
+        # now we want to alter the qml/sld content for all the styles connected with this layers
         ids = list(styles.loc[styles["f_table_name"] == layer_name].index)
-        for id in ids:
+        for id in ids:  # we dont know beforehand if the specific field was used or not. we cycle on all
             qml = styles.loc[id, "styleQML"]
-    
-            import xml.etree.ElementTree as ET
-            tree = ET.fromstring(qml)
-            renderer = tree.find("renderer-v2")
-            
-            if renderer.attrib["attr"] == old_field_name: # the field was used in this style
-                renderer.attrib["attr"] = new_field_name
-                newstyle = str(ET.tostring(tree, encoding='unicode', method='xml'))
-                styles.loc[id,"styleQML"] = newstyle
-            else: # nothing to do - not affected
-                #log.debug("the field were not used as filter for the style")
-                print("the field were not used as filter for the style")
-                return self
-    
-            # clear this because we cannot grant 1 to 1 with qml for now
-            styles.loc[id, "styleSLD"] = ' '
+            newstyle = fix_qml_style_on_field_name_change(qml, old_field_name, new_field_name)
+            styles.loc[id, "styleQML"] = newstyle
+
+            sld = styles.loc[id, "styleSLD"]
+            newstyle = fix_sld_style_on_field_name_change(sld, old_field_name, new_field_name)
+            styles.loc[id, "styleSLD"] = newstyle
             return self
     
    
