2a3712cd5667f48592fc22114c8cb737e7e358c6
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-05-17 12:42:13 +0200

    Add ode formatting/parsing functions

--- gpt/search/ode.py
+++ gpt/search/ode.py
@@ -81,44 +81,105 @@ def search(bbox, target, ihid, iid, pt, match='contain'):
     return products
 
 
-def parse_products(resjs, meta_selectors=None, data_selectors=None, exclude_notes=True):
+def to_geodataframe(products: list, geometry_field='Footprint_C0_geometry',
+                    exclude_footprints=True, unstack_field='product_files'):
+    import geopandas
+    import shapely
+
+    assert isinstance(products, list)
+
+    _products = []
+    for product in products:
+
+        fields_to_exclude = set()
+        content_to_unstack = None
+        if unstack_field:
+            content_to_unstack = product[unstack_field]
+            fields_to_exclude.add(unstack_field)
+        if exclude_footprints:
+            fields_to_exclude.update(set(filter(lambda f:'footprint' in f.lower(), product.keys())))
+
+        _product = {k:v for k,v in product.items() if k not in fields_to_exclude}
+
+        if geometry_field and geometry_field in product:
+            geometry = product[geometry_field]
+            try:
+                geometry = shapely.wkt.loads(geometry)
+            except TypeError as err:
+                raise err
+            _product['geometry'] = geometry
+
+        if content_to_unstack:
+            assert isinstance(content_to_unstack, list)
+            _products.extend([ dict(_product, **ukw) for ukw in content_to_unstack ])
+        else:
+            _products.append(_product)
+
+    gdf = geopandas.GeoDataFrame(_products)
+    return gdf
+
+
+def to_geojson(products: list, geometry_field='Footprint_C0_geometry',
+                exclude_footprints=True, unstack_field='product_files',
+                output_filename=None):
+    import json
+    gdf = to_geodataframe(products, geometry_field, exclude_footprints, unstack_field)
+    gjs = gdf.to_json()
+    assert isinstance(gjs, str)
+    if output_filename:
+        with open(output_filename, 'w') as fp:
+            fp.write(gjs)
+        return output_filename
+    else:
+        return json.loads(gjs)
+
+
+def parse_products(resjs, meta_selectors=None, data_selectors=None,
+                    meta_select_how='filter', data_select_how='all',
+                    include_notes=True, include_footprints=True):
+
+    assert meta_select_how in ('filter','exclude')
+    assert data_select_how in ('any','all'), "Expected to have 'all' or 'any' for 'match_selector'"
+
+    selectors_not_found= set()
     products = []
     for product in resjs:
         meta = {}
-        notes = None
-        files = None
+        notes = None if 'notes' not in product else product['notes']
+        files = None if 'product_files' not in product else product['product_files']
         for key,value in product.items():
+            if include_footprints and 'footprint' in key.lower():
+                meta[key] = value
+                continue
             if key == 'Product_files':
                 files = product[key]['Product_file']
                 if data_selectors:
-                    files = select_files(files, data_selectors)
+                    match_all = True if data_select_how == 'all' else False
+                    files = select_files(files, data_selectors, match_all)
                 continue
             if key == 'ODE_notes':
                 notes = product[key]['ODE_note']
                 continue
-            if not meta_selectors:
-                meta[key] = value
-            else:
-                if key in meta_selectors:
+            if meta_selectors:
+                assert isinstance(meta_selectors, (list,tuple))
+                selectors_not_found.update(set(filter(lambda f:f not in product, meta_selectors)))
+            if (meta_selectors is None
+                or (key in meta_selectors and meta_select_how == 'filter')
+                or (key not in meta_selectors and meta_select_how == 'exclude')):
                     meta[key] = value
 
         meta['product_files'] = files
-        if not exclude_notes:
+        if include_notes:
             meta['notes'] = notes
         products.append(meta)
+
+    if selectors_not_found:
+        print("WARNING: selectors not found:", list(selectors_not_found))
 
     return products
 
 
-def select_files(product_files: list, data_selectors: dict) -> list:
-    try:
-        match_all = data_selectors.pop('match_selector')
-    except:
-        match_all = True
-    else:
-        assert match_all in ('all','any'), "Expected to have 'all' or 'any' for 'match_selector'"
-        match_all = True if match_all == 'all' else False
-
+def select_files(product_files: list, data_selectors: dict, match_all=True) -> list:
     selected_files = []
     for product_file in product_files:
         matches = [ _match_selector(field,value,product_file) for field,value in data_selectors.items() ]
@@ -180,28 +241,28 @@ def _request(url, params):
 
 
 
-def _set_payload(bbox, target=None, host=None, instr=None, ptype=None, how='intersect'):
-    payload = dict(
-        query='product',
-        results='fmpc',
-        output='JSON',
-        loc='f',
-        minlat=bbox['minlat'],
-        maxlat=bbox['maxlat'],
-        westlon=bbox['westlon'],
-        eastlon=bbox['eastlon']
-    )
-
-    if target:
-        payload.update({'target':target})
-    if host:
-        payload.update({'ihid':host})
-    if instr:
-        payload.update({'iid':instr})
-        if ptype:
-            payload.update({'pt':ptype})
-
-    if 'contain' in how:
-        payload.update({'loc':'o'})
-
-    return payload
+# def _set_payload(bbox, target=None, host=None, instr=None, ptype=None, how='intersect'):
+#     payload = dict(
+#         query='product',
+#         results='fmpc',
+#         output='JSON',
+#         loc='f',
+#         minlat=bbox['minlat'],
+#         maxlat=bbox['maxlat'],
+#         westlon=bbox['westlon'],
+#         eastlon=bbox['eastlon']
+#     )
+#
+#     if target:
+#         payload.update({'target':target})
+#     if host:
+#         payload.update({'ihid':host})
+#     if instr:
+#         payload.update({'iid':instr})
+#         if ptype:
+#             payload.update({'pt':ptype})
+#
+#     if 'contain' in how:
+#         payload.update({'loc':'o'})
+#
+#     return payload
