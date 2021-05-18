515a0a9d11c41dfab9608cd75af5f9ac649f8572
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-05-18 11:39:29 +0200

    Add get-items methods to Collection

--- gpt/helpers/collection.py
+++ gpt/helpers/collection.py
@@ -9,38 +9,54 @@ class Collection(object):
 
     def __init__(self, features, index=None):
         if isinstance(features, list):
-            self._gdf = gpd.GeoDataFrame.from_features(features)
+            self._set_data(gpd.GeoDataFrame.from_features(features))
         elif isinstance(features, gpd.GeoDataFrame):
-            self._gdf = features
+            self._set_data(features)
         else:
             raise TypeError(f"Type '{type(features)}' for 'features' not understood.")
-
         if index:
-            self._gdf.set_index(index)
+            self.data.set_index(index)
+
+    @property
+    def data(self):
+        return self._gdf
+
+    def _set_data(self, data):
+        self._gdf = data
+
+    def __getitem__(self, name):
+        return self.data[name]
+
+    # def __setitem__(self, name, value):
+    #     self.data[name] = value
+    #
+    # def __delitem__(self, name):
+    #     del self.data[name]
 
     def __repr__(self):
-        return repr(self._gdf)
+        return repr(self.data)
 
     def __str__(self):
-        return str(self._gdf)
+        return str(self.data)
 
     def __len__(self):
-        return len(self._gdf)
+        return len(self.data)
 
     def __iter__(self):
-        return iter(self._gdf)
-
+        return iter(self.data)
 
     def _get_field(self, column):
-        return self._gdf[column]
-
+        return self.data[column]
 
     def _get_record(self, row):
-        return self._gdf.loc[row]
+        return self.data.loc[row]
 
+    @property
+    def fields(self):
+        return self.data.columns
 
     def _add_field(self, name, data, inplace=True):
-        df = self._gdf
+        df = self.data
         df[name] = data
         if inplace:
             self._gdf = df
@@ -48,24 +64,20 @@ class Collection(object):
             return self.__class__(df)
 
     def deref(self, expr: str):
-        df = self._gdf
+        df = self.data
         res = df.apply(lambda row:expr.format(**{k:v for k,v in row.items()}),
                        axis=1)
         return res
 
-    @property
-    def fields(self):
-        return self._gdf.columns
+    def to_geojson(self, filename=None):
+        if not filename:
+            return json.loads(self.data.to_json())
+
+        self.data.to_file(filename, driver='GeoJSON')
+        return filename
 
     @classmethod
     def from_geojson(cls, filename):
         with open(filename, 'r') as fp:
             gjs = json.load(fp)
         return cls(gjs['features'])
-
-    def to_geojson(self, filename=None):
-        if not filename:
-            return json.loads(self._gdf.to_json())
-
-        self._gdf.to_file(filename, driver='GeoJSON')
-        return filename
